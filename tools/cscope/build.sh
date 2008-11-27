abd89c8c0503703957f05fc3c195e60246ee620c
zealcook <zealcook@dacd16c4-bc18-11dd-824f-6f77523b18d3>
2008-11-27 01:25:19 +0000

    fix bugs

--- tools/cscope/build.sh
+++ tools/cscope/build.sh
@@ -3,8 +3,11 @@
 # cscope for source code reading.
 #
 
-if [ `which cscope` == "" ] ; then
-	echo "Please install cscope first."
+CURRDIR=`pwd`
+TOOLNAME=`basename $CURRDIR`
+
+if [ `which $TOOLNAME`x = ""x ]; then
+	echo "Please install <$TOOLNAME> first."
 	exit 1
 fi
 
@@ -22,9 +25,8 @@ if [ -e ~/.vim/plugin/cscope_maps.vim ]; then
 	exit 1
 else
 	# TODO: if the file has a bad symbol, also will get here.
-	DIR=`pwd`
 	cd ~/.vim/plugin
 	# do not copy it, I want to use the same one everywhere.
-	ln -s $DIR/cscope_maps.vim cscope_maps.vim
+	ln -s $CURRDIR/cscope_maps.vim cscope_maps.vim
 	exit 1
 fi
