b82706397020a039d34978afc428e0f5dfa1f47f
zealcook <zealcook@dacd16c4-bc18-11dd-824f-6f77523b18d3>
2008-11-27 00:56:30 +0000

    update

--- tools/cscope/build.sh
+++ tools/cscope/build.sh
@@ -1,5 +1,20 @@
 #!/bin/sh
-TOOLPATH = `which cscope`
-if [ -x TOOLPATH ] ; then
-	echo "x"
+if [ `which cscope` == "" ] ; then
+	echo "Please install cscope first."
+	exit 1
+fi
+
+if [ ! -e ~/.vim/plugin ]; then
+	mkdir -p ~/.vim/plugin
+fi
+
+if [ -e ~/.vim/plugin/cscope_maps.vim ]; then
+	echo "###"
+	echo "#	Have a older version in your system. We don't override it."
+	echo "#	If your are sure what you are doing, please remove the older one first."
+	echo "#	Then excute this script again."
+	echo "###"
+	exit 1
+else
+	cp cscope_maps.vim ~/.vim/plugin
 fi
