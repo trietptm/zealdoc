77a281d071acc6494cbd5e908d51defbe009b3ef
zealcook <zealcook@dacd16c4-bc18-11dd-824f-6f77523b18d3>
2008-12-02 10:58:42 +0000

    fix

--- project/os/include/global.h
+++ project/os/include/global.h
@@ -8,14 +8,14 @@
 #endif
 
 EXTERN	int		disp_pos;
-EXTERN	DESCRIPTOR	gdt[GDT_SIZE];
 EXTERN	t_8		gdt_ptr[6];	/* 0~15:Limit  16~47:Base */
-EXTERN	GATE		idt[IDT_SIZE];
 EXTERN	t_8		idt_ptr[6];	/* 0~15:Limit  16~47:Base */
+EXTERN	DESCRIPTOR	gdt[GDT_SIZE];
+EXTERN	GATE		idt[IDT_SIZE];
 
 /* kliba.asm */
-PUBLIC void	out_byte(t_port port, t_8 value);
 PUBLIC t_8	in_byte(t_port port);
+PUBLIC void	out_byte(t_port port, t_8 value);
 PUBLIC void	disp_str(char * info);
 PUBLIC void	disp_color_str(char * info, int color);
 PUBLIC void	init_prot();
