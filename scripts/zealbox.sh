57bea62852caa9ac9e3d88033c5203721e8010f1
zealcook <zealcook@dacd16c4-bc18-11dd-824f-6f77523b18d3>
2008-12-06 05:50:34 +0000

    fix bugs

--- scripts/zealbox.sh
+++ scripts/zealbox.sh
@@ -6,16 +6,27 @@
 #
 function fxg()
 {
-	echo "in fxg $1"
-	exit
+	if [ $# -ne 1 ]; then
+		echo 'Usage: fxg $word_you_find'
+		exit 1
+	fi
 	find . -name .svn -prune -o -name .pc -prune -o -name CVS -prune -o -print | xargs grep $1
+	return 0
 }
 
-if [ "$1"x = ""x ]; then
-	echo "no grep param"
-	exit 1;
+function check_argc()
+{
+	if [ "$1"x = ""x ]; then
+		echo "no grep param"
+		exit 1;
+	fi
+}
+
+cmd=`basename $0`
+$cmd $* 2>/dev/null
+
+# command return 0 when finished.
+if [ $? -ne 0 ]; then
+	echo "command not found"
+	exit
 fi
-echo $0 $1
-cmd=$0
-$1 a
-exit
