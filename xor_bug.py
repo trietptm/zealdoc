354b286b2bb8eaaad40c621b9130a2f0f8f2b30f
zealcook <zealcook@dacd16c4-bc18-11dd-824f-6f77523b18d3>
2009-06-29 09:15:35 +0000

    bug of python print string

--- xor_bug.py
+++ xor_bug.py
@@ -7,13 +7,16 @@ from Crypto.Cipher import XOR
 def str2hex(s):
 	return "".join([string.zfill(hex(ord(x))[2:], 2)
 		for x in tuple(s)])
-key = "atbgnxv"
-src = "xxxxxxx"
+key = "atbnxv"
+src = "xxxxxx"
 obj = XOR.new(key)
 en = obj.encrypt(src)
 de = obj.decrypt(en)
+en2 = '\x19\x0c\x1a\x16\x00\x0e'
+print "%s" % en2
 
 print "key is %s" % key
 print "src is %s" % src
-print "en is %s/%s" % (en, str2hex(en))
+print "en is %s" % (str2hex(en))
+#print "en is %s/%s" % (en, str2hex(en))
 print "de is %s/%s" % (de, str2hex(de))
