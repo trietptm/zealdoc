e80ed4412bc1eb47693227821047cea2a61cca62
zealcook <zealcook@dacd16c4-bc18-11dd-824f-6f77523b18d3>
2011-01-25 08:52:03 +0000

    update

--- configs/cscope/build.sh
+++ configs/cscope/build.sh
@@ -2,18 +2,14 @@
 #
 
 CURRDIR=`pwd`
-TOOLNAME=`basename $CURRDIR`
 
-source ../../lib/lib.sh
+if [ ! -e ~/.vim/plugin ]; then
+	mkdir -p ~/.vim/plugin
+fi
 
-cmd_not_exist_exit $TOOLNAME;
+if [ ! -e ~/.vim/plugin/cscope_maps.vim ]; then
+	cd ~/.vim/plugin
+	ln -s $CURRDIR/cscope_maps.vim cscope_maps.vim
+fi
 
-dir_not_exist_create ~/.vim/plugin;
-
-file_exist_exit ~/.vim/plugin/cscope_maps.vim;
-sym_file_exist_exit ~/.vim/plugin/cscope_maps.vim;
-
-cd ~/.vim/plugin
-# do not copy it, I want to use the same one everywhere.
-ln -s $CURRDIR/cscope_maps.vim cscope_maps.vim
 exit 1
